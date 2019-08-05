a0976cc2b2e116213fc4990839ddd49d863b1fd9
Megan Duclos <megan-duclos@ip-172-18-4-148.us-west-2.compute.internal>
2019-08-05 15:37:17 -0600

    Manage program flow, calculate letter grade

--- cs/gradebook/src/GradeBook/Book.cs
+++ cs/gradebook/src/GradeBook/Book.cs
@@ -10,9 +10,43 @@ namespace GradeBook
             grades = new List<double>();
             Name = name;
         }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                    AddGrade(60);
+                    break;
+                
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else 
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
 
         public Statistics GetStatistics()
@@ -30,6 +64,29 @@ namespace GradeBook
             }
             result.Average /= grades.Count;
             
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
             return result;
         }
 
