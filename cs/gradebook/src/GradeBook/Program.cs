478ee301b8c840ac39f55cd5b07c518ad5b8c486
Megan Duclos <megan-duclos@ip-172-18-6-90.us-west-2.compute.internal>
2019-08-26 16:33:48 -0600

    OOP with CS

--- cs/gradebook/src/GradeBook/Program.cs
+++ cs/gradebook/src/GradeBook/Program.cs
@@ -6,18 +6,28 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book("Megan's Grade Book");
-            book.GradeAdded += OnGradeAdded;
-            book.GradeAdded += OnGradeAdded;
+            IBook book = new DiskBook("Megan's Grade Book");
             book.GradeAdded += OnGradeAdded;
 
-            
-            while(true)
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+        }
+
+        private static void EnterGrades(IBook book)
+        {
+            while (true)
             {
                 Console.WriteLine("Enter a grade or 'q' to quit");
                 var input = Console.ReadLine();
-                
-                if(input == "q")
+
+                if (input == "q")
                 {
                     break;
                 }
@@ -28,21 +38,19 @@ namespace GradeBook
                     book.AddGrade(grade);
 
                 }
-                catch(Exception ex)
+                catch (ArgumentException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-
-
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
             }
-
-            var stats = book.GetStatistics();
-
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
-            Console.WriteLine($"The average grade is {stats.Average:N1}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
-            
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
--- cs/gradebook/src/test/GradeBook.Tests/TypeTests.cs
+++ cs/gradebook/src/test/GradeBook.Tests/TypeTests.cs
@@ -62,9 +62,9 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void GetBookSetName(ref Book book, string name)
+        private void GetBookSetName(ref InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
         [Fact]
@@ -76,9 +76,9 @@ namespace GradeBook.Tests
             Assert.Equal("Book 1", book1.Name);
         }
 
-        private void GetBookSetName(Book book, string name)
+        private void GetBookSetName(InMemoryBook book, string name)
         {
-            book = new Book(name);
+            book = new InMemoryBook(name);
         }
 
         [Fact]
@@ -90,7 +90,7 @@ namespace GradeBook.Tests
             Assert.Equal("New Name", book1.Name);
         }
 
-        private void SetName(Book book, string name)
+        private void SetName(InMemoryBook book, string name)
         {
             book.Name = name;
         }
@@ -131,9 +131,9 @@ namespace GradeBook.Tests
             Assert.Same(book1, book2);
         }
 
-        private Book GetBook(string name)
+        private InMemoryBook GetBook(string name)
         {
-            return new Book(name);
+            return new InMemoryBook(name);
         }
     }
 }
